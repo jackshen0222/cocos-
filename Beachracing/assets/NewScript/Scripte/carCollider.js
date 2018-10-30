00068e0ba98cdc14be8137d4668337c5a777963f
xc <791937096@qq.com>
2018-10-30 17:46:24 +0800

    Revert "取消了变更位置"

--- Beachracing/assets/NewScript/Scripte/carCollider.js
+++ Beachracing/assets/NewScript/Scripte/carCollider.js
@@ -40,7 +40,7 @@ cc.Class({
         var selfSp = self.getComponent(cc.Sprite)
         //碰撞其他车辆
         if (other.node.group === 'CarCollider' && self.node.group === 'CarCollider') {
-            if (selfSp.spriteFrame === otherSp.spriteFrame && selfSp.spriteFrame.name !== 'Money2') {//车辆合成
+            if (selfSp.spriteFrame === otherSp.spriteFrame && selfSp.spriteFrame.name !== 'Money2' ) {//车辆合成
                 for (let i = 0; i < carArr.length; i++) {
                     if (selfSp.spriteFrame === carArr[i].getComponent(cc.Sprite).spriteFrame) {
                         if (carArr[i] === carArr[9]) { return; }//最高等级时不能合成
@@ -49,16 +49,17 @@ cc.Class({
                         carArr[i + 1].active = true//下一等级图片显示
                     }
                 }
-                /************************************* */
+                self.node.active = false;//self被碰的隐藏,other-拖动的不隐藏
+
+                // /************************************* */
                 // //self.SpriteFrame需要加载回原样，节点才可以正确隐藏还原
                 // cc.loader.loadRes('Money2', cc.SpriteFrame, function (err, res) {
                 //     selfSp.spriteFrame = res
                 // })
-                self.node.active = false;//self被碰的隐藏,other-拖动的不隐藏
-
             }
+                
 
-            /*********************************** */
+            // /*********************************** */
             // //变更位置 
             // //在car.js 中的鼠标拖放End事件中，让节点全部显示，之后购买车辆时，再将图片为空的隐藏
             // if (selfSp.spriteFrame.name === 'Money2') {
@@ -66,7 +67,7 @@ cc.Class({
             //         if (otherSp.spriteFrame === carArr[i].getComponent(cc.Sprite).spriteFrame) {
             //             this.scheduleOnce(function(){
             //                 selfSp.spriteFrame = carArr[i].getComponent(cc.Sprite).spriteFrame
-            //             },0.01)
+            //             },0.02)
             //         }
             //     }
             //     cc.loader.loadRes('Money2', cc.SpriteFrame, function (err, res) {
